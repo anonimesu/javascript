c49cfc9d9f7947388bd327098b49967cba08b541
anonimesu <30668897+anonimesu@users.noreply.github.com>
2017-08-10 17:05:44 -0400

    practice

---
+++ hello.js
@@ -0,0 +1,36 @@
+var favColor = 'red';
+var myArray = ['car, truck, jeep'];
+var numofArrary = 3;
+var richObject = {
+
+	fistName: 'Grant';
+	lastName: 'Tom'
+
+
+
+
+}
+
+
+var whatisMyFavColor = function () {
+
+
+
+}
+
+var dosomething = function () {
+
+	console.log('do something!');
+
+}
+
+var area = function (width, height) {
+
+	return width * height;
+}
+
+var fullname = function (firstname, secondname){
+
+	return firstname + '' + secondname;
+
+}
